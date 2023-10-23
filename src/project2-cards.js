223d53293331d1f447ae1c2f44dd71366a446d51
declankennedy14 <declankennedy1410@gmail.com>
2023-10-22 20:44:10 -0400

    project 2 check in

---
+++ src/project2-cards.js
@@ -0,0 +1,3 @@
+import { project2cards } from './project2cards.js';
+
+customElements.define('project2cards', project2cards);
--- src/project2cards.js
+++ src/project2cards.js
@@ -1,6 +1,6 @@
 import { LitElement, html, css } from 'lit';
 
-export class PolarisChip extends LitElement {
+export class project2cards extends LitElement {
   static get properties() {
     return {
       title: { type: String },
